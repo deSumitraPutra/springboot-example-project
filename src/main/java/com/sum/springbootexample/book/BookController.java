bfff64fb7ad60382768a7b2a740af9051b2165ab
deSumitraPutra <i.dw.pt.sumitra.putra@gmail.com>
2024-11-27 19:56:59 +0700

    add rest controller

--- src/main/java/com/sum/springbootexample/book/BookController.java
+++ src/main/java/com/sum/springbootexample/book/BookController.java
@@ -2,13 +2,13 @@ package com.sum.springbootexample.book;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @AllArgsConstructor
 @RequestMapping("/books")
 public class BookController {
